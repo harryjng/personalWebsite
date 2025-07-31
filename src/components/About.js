afef569e74f22f5495d72f08bbf78aaa52026d95
Harry Jung <harryjung@mac.mynetworksettings.com>
2025-07-31 17:56:52 -0400

    finished about me. added hyperlinks, coloring when hover, bolding

--- src/components/About.js
+++ src/components/About.js
@@ -5,20 +5,20 @@ export default function Hero() {
         <section id="AboutMe" className="about-section">
             <div className='about'>
                 <p>
-                    I’m a software engineer with a strong foundation in <span className='highlight-word'>full-stack development</span>, 
-                    <span className='highlight-word'> machine learning</span>, and <span className='highlight-word'>human-centered design</span>. I’m passionate about building impactful tools 
-                    from secure <span className='highlight-word'>mobile apps</span> and AI-powered systems to real-time mapping platforms that solve real-world problems.
+                    I’m a software engineer with a strong foundation in <a href="https://www.akima.com/opcos/cloud-lake-technology-data-analytics/" target="_blank" rel="noopener noreferrer" className='highlight-word'>full-stack development</a>, 
+                    <a href="https://github.com/AwrodHaghiTabrizi/UMARV-CV-LaneDetection" target="_blank" rel="noopener noreferrer" className='highlight-word'> machine learning</a>, and <a href="https://zetapi.tech" target="_blank" rel="noopener noreferrer" className='highlight-word'>human-centered design</a>. I’m passionate about building impactful tools 
+                    from secure <a href="https://github.com/HostYourVoice/Skilt" target="_blank" rel="noopener noreferrer" className='highlight-word'>mobile apps</a> and AI-powered systems to real-time mapping platforms that solve real-world problems.
                 </p>
                 <p>
-                    Currently, I’m a Software Engineer at <span className='highlight-word'>Akima</span>, where I support federal 
+                    Currently, I’m a Software Engineer at <a href="http://akima.com" target="_blank" rel="noopener noreferrer" className='highlight-word'>Akima</a>, where I support federal 
                     clients by building internal tools. I recently delivered an XR-based construction 
                     visualization tool, as well as a military-grade emergency response platform. I hold a Secret-level clearance 
                     and enjoy contributing to engineering in the public sector.
                 </p>
                 <p>
-                    My work spans startups, labs, and <span className='highlight-word'>nonprofit</span> settings. At the <span className='highlight-word'>Real time Computing Lab</span>, I've developed 
-                    algorithms to reduce shoulder surfing. I’ve also led <span className='highlight-word'>full-stack projects</span> for organizations, improved 
-                    automation, and built several AI-powered tools—from a <span className='highlight-word'>colorization model</span> to a real-time NBA 
+                    My work spans startups, labs, and <a href="http://akpsports.org" target="_blank" rel="noopener noreferrer" className='highlight-word'>nonprofit</a> settings. At the <a href="https://rtcl.eecs.umich.edu/rtclweb/" target="_blank" rel="noopener noreferrer" className='highlight-word'>Real time Computing Lab</a>, I've developed 
+                    algorithms to reduce shoulder surfing. I’ve also led <a href="https://www.linkedin.com/company/wolverinesportsanalytics/posts/?feedView=all" target="_blank" rel="noopener noreferrer" className='highlight-word'>full-stack projects</a> for organizations, improved 
+                    automation, and built several AI-powered tools—from a <a href="https://github.com/mandyschen/BWToColorized" target="_blank" rel="noopener noreferrer" className='highlight-word'>colorization model</a> to a real-time NBA 
                     performance predictor.
 
                 </p>
