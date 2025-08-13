6651ce9b20c35b1c7235468000615ae126e88bdb
Harry <harryjng@umich.edu>
2025-08-13 19:47:22 -0400

    finishing experience for mobile and starting on project for mobile

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,7 +3,7 @@ export default function Project() {
   return (
     <div className="project-container">
       <section id = "Projects"></section>
-      <h1>Project</h1>
+      <h1>PROJECTS</h1>
     </div>
   );
 }
