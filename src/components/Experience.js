af3f8c27f38ced3f206f30a26bdc61a882ad3f71
Harry <harryjng@umich.edu>
2025-08-13 16:50:53 -0400

    adding headers to mobile layout

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -5,7 +5,7 @@ export default function Experience() {
     <>
       {/* First Experience - Software Engineer at Akima */}
       <section id="Experience" className="experience-section group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
-        <a href="https://www.linkedin.com/company/cloud-lake-technology/" target="_blank"
+<h1 className="experience-heading">EXPERIENCE</h1>        <a href="https://www.linkedin.com/company/cloud-lake-technology/" target="_blank"
           rel="noopener noreferrer"
           className="contents">
           <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg" />
