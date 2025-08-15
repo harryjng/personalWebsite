f387115362fc805354f9f88da11b80129ed70282
Harry <harryjng@umich.edu>
2025-08-15 16:17:43 -0400

    added box effect to project

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,18 +5,16 @@ export default function Project() {
   return (
     <>
       <div className='project-container'>
-        <section className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4">
+        <section className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
           <a href="https://www.youtube.com/watch?v=SOMgoDBU5QQ" target="_blank"
             rel="noopener noreferrer"
             className="contents">
 
-            {/* The following div was removed to fix the dimming effect */}
-            {/* <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg lg:group-hover/list:opacity-50" />
-            */}
+            <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg" />
 
-            <img src={WordSmith} alt="Project Logo" className='project-images' />
+            <img src={WordSmith} alt="Project Logo" className='project-images z-10 relative' />
 
-            <h3 className="font-medium leading-snug text-slate-200">
+            <h3 className="font-medium leading-snug text-slate-200 z-10 relative">
               <div>
                 <span className="inline-flex items-baseline font-medium leading-tight text-slate-200 group-hover:text-teal-300 focus-visible:text-teal-300 group/link text-base relative">
                   <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block" />
@@ -40,7 +38,7 @@ export default function Project() {
               </div>
             </h3>
 
-            <p className="mt-2 text-sm leading-normal text-slate-400 group-hover:!text-slate-400 text-base leading-relaxed font-sans antialiased font-medium">
+            <p className="mt-2 text-sm leading-normal text-slate-400 text-base leading-relaxed font-sans antialiased font-medium z-10 relative">
               Built a modular course system on Supabase with AI writing validation, Elo-based progress tracking, and voice/OCR integration using SwiftUI, Python, and OpenAI API.
             </p>
           </a>
